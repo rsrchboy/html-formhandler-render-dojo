d2d79063bf56bea519191acedd2abece75307fda
Chris Weyl <cweyl@alumni.drew.edu>
2011-10-15 23:49:24 -0700

    CDN roles: don't include render roles

--- lib/HTML/FormHandler/Render/Dojo/CDN/AOL.pm
+++ lib/HTML/FormHandler/Render/Dojo/CDN/AOL.pm
@@ -5,8 +5,6 @@ package HTML::FormHandler::Render::Dojo::CDN::AOL;
 use Moose::Role;
 use namespace::autoclean;
 
-with 'HTML::FormHandler::Render::Dojo';
-
 # AFAIK only http is supported (https caused invalid cert complaints)
 sub dojo_base { 'http://o.aolcdn.com/dojo' }
 
--- lib/HTML/FormHandler/Render/Dojo/CDN/Google.pm
+++ lib/HTML/FormHandler/Render/Dojo/CDN/Google.pm
@@ -5,8 +5,6 @@ package HTML::FormHandler::Render::Dojo::CDN::Google;
 use Moose::Role;
 use namespace::autoclean;
 
-with 'HTML::FormHandler::Render::Dojo';
-
 sub dojo_base    { '//ajax.googleapis.com/ajax/libs/dojo' }
 
 !!42;
